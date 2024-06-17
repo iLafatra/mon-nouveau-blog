85ae58d9aa52edbc29e3effab476aa8207a319aa
lafatra <lafatrandrianina@gmail.com>
2024-06-17 16:10:28 +0300

    My Django Girls app, first commit

---
+++ initiation.py
@@ -0,0 +1,14 @@
+#variable 
+a=0
+nom="lafatra"
+tab=[0,1,2,3]
+dictionnaire: {
+    "lafatra":19,
+    "filamatra":22
+}
+def produit(tab):
+    c=tab[1]*tab[3]
+    return c
+
+c = produit(tab)
+print(c)
